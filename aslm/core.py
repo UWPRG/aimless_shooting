38e2368bbd59dee3c1911992d44d448b0d684f92
Sarah Alamdari <salamda1@uw.edu>
2018-09-17 15:03:06 -0700

    core.py

--- aslm/core.py
+++ aslm/core.py
@@ -1,2 +1,9 @@
+import os 
+
+def run_MD(name,inputfile,prmtop,logfile):
+    """
+    Function takes a name and runs MD
+    """
+    sander -O -i inputfile.in -o name.out -x name.nc -p system.prmtop -c name.rst7 -r name_out.rst7 -inf name.info >> log.log
 
 
