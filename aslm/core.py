4abe1abb5bc662720701f0c4194bbdfa80d0fcc4
Sarah Alamdari <salamda1@uw.edu>
2018-09-25 13:33:27 -0700

    start adding files for plumed

--- aslm/core.py
+++ aslm/core.py
@@ -56,6 +56,27 @@ def find_and_replace(line, substitutions):
     replace = regex.sub(lambda match: substitutions[match.group(0)], line)
     return replace
 
+def create_plumed_file(atoms, plumed_file='plumed.dat',
+                       plumed_template='plumed_template.dat'):
+    """
+    Function will take a list of atoms, COMS, and groups and create a plumed
+    file containing distances, differences of distances, angles, dihedrals,
+    and other interesting CVs
+
+    Parameters
+    ----------
+    atoms : list
+        Given a list of important atoms, function will generate a plumed entry
+        for each set of distances
+
+    """
+    # create CVs 
+
+    # define basin 
+
+    # print settings
+
+    # COLVAR name
 
 class ShootingPoint:
     def __init__(self):
