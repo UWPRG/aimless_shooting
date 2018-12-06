549430ab02c82c51fff5de869f152318e0bdab9f
Sarah Alamdari <salamda1@uw.edu>
2018-12-06 10:30:33 -0800

    add plumed file

---
+++ aslm/plumed.py
@@ -0,0 +1,21 @@
+def gen_plumed_file(atoms, plumed_file='plumed.dat',
+                       plumed_template='plumed_template.dat'):
+    """
+    Function will take a list of atoms, COMS, and groups and create a plumed
+    file containing distances, differences of distances, angles, dihedrals,
+    and other interesting CVs
+
+    Parameters
+    ----------
+    atoms : list
+        Given a list of important atoms, function will generate a plumed entry
+        for each set of distances
+
+    """
+    # create CVs 
+
+    # define basin 
+
+    # print settings
+
+    # COLVAR name
