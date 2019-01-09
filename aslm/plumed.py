532c28d827e7cad0a2f126c3b2011066ef54a6d3
sarahalamdari <salamda1@uw.edu>
2019-01-08 16:07:37 -0800

    functional plumed generation file

--- aslm/plumed.py
+++ aslm/plumed.py
@@ -1,6 +1,6 @@
 from itertools import combinations
 
-def gen_plumed_file(atoms,basin_CV,print_stride='1',plumed_file='plumed.dat',
+def gen_plumed_file(atoms,basins,basin_CV,limit,print_stride='1',plumed_file='plumed.dat',
                        plumed_outfile='COLVAR'):
     """
     Function will take a list of atoms, COMS, and groups and create a plumed
@@ -13,11 +13,23 @@ def gen_plumed_file(atoms,basin_CV,print_stride='1',plumed_file='plumed.dat',
         Given a list of important atoms, function will generate a plumed entry
         for each set of distances
         Example: [1,2,3,4]
+    
+    basins : int
+        Number of basins used to monitor COMMITTOR
 
     basin_CVs : list of strings
         List of variables that correspond to the CVs to be monitored for each 
         basin. Only accepts 1-2 CVs as an input.
         Example: ['phi','psi']
+
+    limit : array 
+        Array of integers that correspond to the lower and upper limits for 
+        each basin. Must be enetered in a specific format
+        Example for 2 basins: [[LL1_x,LL1_y,UL1_x,UL1_y],[LL2_x,LL2_y,UL2_x,UL2_y]]
+        * where x and y correspond to those values for each supplied argument
+        * 1 and 2 correspond to each basin
+        * LL = lower limit 
+        * UL = upper limit
 
     """
     # Create pairs of 2 to describe distance
@@ -52,15 +64,27 @@ def gen_plumed_file(atoms,basin_CV,print_stride='1',plumed_file='plumed.dat',
         print("Must include at least two atoms in the atom namelist to generate distances")
 
     f.write('\nCOMMITTOR ...\n')
-    if len(basin_CV) == 1:
-        f.write('ARG={}\n'.format(basin_CV[0]))
-    if len(basin_CV) == 2:
-        f.write('ARG={},{}\n'.format(basin_CV[0],basin_CV[1]))
-    if len(basin_CV) >= 3:
-        print ("Use of more than 2 CVs is not supported in this script", 
+    if basins == 2:
+        f.write('ARG={}\n'.format(basin_CV[0], basin_CV[1]))
+        f.write('STRIDE={}\n'.format(print_stride))
+        f.write('BASIN_LL1={},{}\n'.format(limit[0][0], limit[0][1]))
+        f.write('BASIN_UL1={},{}\n'.format(limit[0][2], limit[0][3]))
+        f.write('BASIN_LL2={},{}\n'.format(limit[1][0], limit[1][1]))
+        f.write('BASIN_UL2={},{}\n'.format(limit[1][2], limit[1][3]))
+    if basins == 3:
+        f.write('ARG={},{}\n'.format(basin_CV[0],basin_CV[1],basin_CV[3]))
+        f.write('STRIDE={}\n'.format(print_stride))
+        f.write('BASIN_LL1={},{}\n'.format(limit[0][0], limit[0][1]))
+        f.write('BASIN_UL1={},{}\n'.format(limit[0][2], limit[0][3]))
+        f.write('BASIN_LL2={},{}\n'.format(limit[1][0], limit[1][1]))
+        f.write('BASIN_UL2={},{}\n'.format(limit[1][2], limit[1][3]))
+        f.write('BASIN_LL3={},{}\n'.format(limit[2][0], limit[2][1]))
+        f.write('BASIN_UL3={},{}\n'.format(limit[2][2], limit[2][3]))
+    if len(basin_CV) >= 4:
+        print ("Use of more than 3 CVs to monitor COMMITTOR is not currently supported", 
                 "you may manually edit the plumed file")
-    if len(basin_CV) < 1:
-        print ("You must provide a CV to monitor if the shooting point has committed to a basin")
+    if len(basin_CV) < 2:
+        print ("You must provide at least two CVs to monitor if the shooting point has committed to a basin")
     f.write('... COMMITTOR\n')
 
     f.write('\nPRINT STRIDE={} ARG=* FILE={}'.format(print_stride,plumed_outfile))
